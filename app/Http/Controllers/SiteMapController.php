5c128ad213fbcc7125792b31d706b6686f0f96e4
HiroyukiYahagi <yahagi1989@gmail.com>
2018-01-10 17:17:14 +0900

    sitemap

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -12,8 +12,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Services\ArticleService;
-use App\Services\CategoryService;
 use Carbon\Carbon;
 use App;
 
@@ -22,39 +20,9 @@ use App;
  */
 class SiteMapController extends Controller
 {
-	/**
-     * 記事サービス
-     * @var App\Services\ArticleService
-     */
-    protected $articleService;
-
-    /**
-     * カテゴリサービス
-     * @var App\Services\CategoryService
-     */
-    protected $categoryService;
-  
-  	/**
-     * コンストラクタ
-     *
-     * @access public
-     * @param App\Services\ArticleService $articleService 記事サービス
-     * @param App\Services\CategoryService $categoryService カテゴリサービス
-     */
-    public function __construct( ArticleService $articleService, 
-        CategoryService $categoryService){
-        $this->articleService = $articleService;
-        $this->categoryService = $categoryService;
+    public function __construct(){
     }
 
-    /**
-     * サイトマップの出力
-     *
-     * キャッシュに保存し、期限切れになったら再生成する。
-     *
-     * @access public
-     * @return Illuminate\Http\Response 
-     */
     public function index(){
 		$sitemap = App::make('sitemap');
 		$sitemap->setCache('laravel.sitemap', 60);
